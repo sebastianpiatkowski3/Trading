e111cea1e8dd218eacc70a2d155fa4363b73174a
sebastianpiatkowski3 <sebastianpiatkowski73@gmail.com>
2024-01-16 23:20:42 +0100

    Dodałem klasę Magazyn, wyświetla na stronie stan magazynu.

--- app.py
+++ app.py
@@ -23,13 +23,76 @@ Zapewnij przyjazny dla użytkownika interfejs, stosując style CSS.
 '''
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from flask_bootstrap import Bootstrap
+from magazyn import Magazyn
 
 app = Flask(__name__)
 
-@app.route('/')
+pp = Flask(__name__)
+bootstrap = Bootstrap(app)
+
+appHasRunBefore: bool = False  # w celu wykonania fragmentu kodu tylko raz w całym cyklu życia tego programu.
+
+magazyn = Magazyn()
+"""
+     Metody klasy Magazyn:
+             == "1":
+                self.module_saldo()
+             == "2":
+                self.module_sprzedaz()
+             == "3":
+                self.module_zakup()
+             == "4":
+                self.module_konto()
+             == "5":
+                self.module_lista()
+             == "6":
+                self.module_magazyn()
+             == "7":
+                self.module_przeglad()
+             == "9":
+                self.save_balance()
+                self.save_history()
+                self.save_inventory()
+             == "0":
+                self.save_balance()
+                self.save_history()
+                self.save_inventory()
+"""
+@app.before_request
+def firstRun():
+    global appHasRunBefore
+    if not appHasRunBefore:
+        appHasRunBefore = True
+        print("Ten kod zostanie wykonany tylko raz w całym cyklu życia tego programu.")
+        magazyn.load_data()
+
+
+uzytkownicy = {
+    'Anna': 'anna@example.com',
+    'Kuba':'kjh@kjhk.pl',
+    'Jan': 'jan@example.com',
+    'Katarzyna': 'katarzyna@example.com',
+    'Piotr': 'piotr@example.com',
+    'Magdalena': 'magdalena@example.com',
+    'Tomasz': 'tomasz@example.com',
+    'Aleksandra': 'aleksandra@example.com',
+    'Krzysztof': 'krzysztof@example.com',
+    'Ewa': 'ewa@example.com',
+    'Marcin': 'marcin@example.com'
+}
+
+
+@app.route('/', methods=["GET", "POST"])  # Dodajemy metodę POST aby wysłać formularz
 def index():
-    return render_template('index.html')
+    if request.form:  # Sprawdzamy, czy formularz przyszedł (zabezpieczenie przed brakiem formularza na metodzie GET)
+        email = request.form.get("email")  # Wyciągamy pole "email" z formularza
+        haslo = request.form.get("haslo")
+        uzytkownicy[email] = haslo
+    return render_template('index.html',
+                           stany=magazyn.module_lista(),
+                           history=magazyn.module_przeglad() )
 
 
 @app.route('/historia')
